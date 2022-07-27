d943d3eaea8203d47760954dd01c5489994c70b7
Dennis Moes <dennismoes@me.com>
2022-07-27 17:33:32 +0200

    feat: :building_construction: Added a bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:musicbox/home.dart';
 import 'package:musicbox/playlist.dart';
@@ -13,8 +14,22 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+  List<Widget> _pages = [HomeScreen(), PlaylistScreen()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,15 +39,37 @@ class MyApp extends StatelessWidget {
         appBarTheme: const AppBarTheme(
           backgroundColor: Color(0xFFFAFAFA),
           elevation: 1,
+          centerTitle: false,
           titleTextStyle: TextStyle(
             color: Colors.deepPurple,
             fontSize: 20,
             fontWeight: FontWeight.bold,
           ),
+          actionsIconTheme: IconThemeData(
+            color: Colors.deepPurple,
+          ),
         ),
       ),
-      home: HomeScreen(),
-      // home: PlaylistScreen(),
+      home: Scaffold(
+        body: IndexedStack(
+          index: _selectedIndex,
+          children: _pages,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.music_note_list),
+              label: 'Playlist',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
