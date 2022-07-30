0afd26d003c33e498020a3b2b37aea55f965eb88
Dennis Moes <dennismoes@me.com>
2022-07-30 15:18:52 +0200

    feat: :lipstick: Added a image for the favorite section

--- lib/playlist.dart
+++ lib/playlist.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:musicbox/components/add_playlist.dart';
 import 'package:musicbox/components/playlist_card.dart';
 import 'package:musicbox/favorite.dart';
 import 'package:musicbox/store_controller.dart';
@@ -14,6 +16,13 @@ class PlaylistScreen extends StatefulWidget {
 
 class _PlaylistScreenState extends State<PlaylistScreen> {
   final storeController = Get.put(StoreController());
+  final String assetName = 'assets/images/bbburst.svg';
+  final Widget svg = SvgPicture.asset(
+    'assets/bbburst.svg',
+    fit: BoxFit.fitWidth,
+    alignment: Alignment.bottomRight,
+    clipBehavior: Clip.hardEdge,
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +31,15 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
         title: const Text("Playlists"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  fullscreenDialog: true,
+                  builder: (context) => const AddPlaylistScreen(),
+                ),
+              );
+            },
             icon: const Icon(CupertinoIcons.add),
           ),
         ],
@@ -41,40 +58,86 @@ class _PlaylistScreenState extends State<PlaylistScreen> {
 
   Widget _favoriteCard() {
     return Padding(
-      padding: const EdgeInsets.all(20),
-      child: AspectRatio(
-        aspectRatio: 21 / 9,
+        padding: const EdgeInsets.all(20),
         child: Material(
-          color: Colors.deepPurple,
-          borderRadius: BorderRadius.circular(10),
           clipBehavior: Clip.hardEdge,
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const FavoriteScreen(),
+          child: AspectRatio(
+            aspectRatio: 21 / 9,
+            child: Material(
+              color: const Color(0xFF06083D),
+              borderRadius: BorderRadius.circular(6),
+              clipBehavior: Clip.hardEdge,
+              child: Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/bbburst.png'),
+                    fit: BoxFit.cover,
+                    alignment: Alignment.topLeft,
+                  ),
                 ),
-              );
-            },
-            child: const Align(
-              alignment: Alignment.bottomRight,
-              child: Padding(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Favorites",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold,
+                child: InkWell(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const FavoriteScreen(),
+                      ),
+                    );
+                  },
+                  child: const Align(
+                    alignment: Alignment.bottomRight,
+                    child: Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Favorites",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
                 ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        )
+
+        // child: AspectRatio(
+        //   aspectRatio: 21 / 9,
+        //   child: Material(
+        //     color: Colors.deepPurple,
+        //     borderRadius: BorderRadius.circular(10),
+        //     clipBehavior: Clip.hardEdge,
+        //     child: svg,
+        //   child: InkWell(
+        //     onTap: () {
+        //       Navigator.push(
+        //         context,
+        //         MaterialPageRoute(
+        //           builder: (context) => const FavoriteScreen(),
+        //         ),
+        //       );
+        //     },
+        //     child: const Align(
+        //       alignment: Alignment.bottomRight,
+        //       child: Padding(
+        //         padding: EdgeInsets.all(10),
+        //         child: Text(
+        //           "Favorites",
+        //           style: TextStyle(
+        //             color: Colors.white,
+        //             fontSize: 22,
+        //             fontWeight: FontWeight.bold,
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        //   ),
+        // ),
+        );
   }
 
   Widget _playlistColumn() {
